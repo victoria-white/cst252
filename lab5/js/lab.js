4c32edec1a267c8a1bea739955b88a5711b8a378
Victoria White <65001478+victoria-white@users.noreply.github.com>
2021-02-14 15:23:26 -0800

    Update 2/14
    added images and code to js file

---
+++ lab5/js/lab.js
@@ -0,0 +1,13 @@
+var carMake = "Prius";
+var carModel = "Toyota";
+var carColor = "Gray";
+var carYear = 2008;
+var currentYear = 2021;
+var carAge = currentYear - carYear;
+var ownIt = "true";
+
+document.writeIn("Make: " + carMake + "<br>");
+document.writeIn("Model: " + carModel + "<br>");
+document.writeIn("Color: " + carColor + "<br>");
+document.writeIn("Year: " + carYear + "<br>");
+document.writeIn("Age: " + carAge + "<br>");
