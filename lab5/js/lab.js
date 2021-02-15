604725ff19d291b07f57409fc54347c7c5e95854
Victoria White <65001478+victoria-white@users.noreply.github.com>
2021-02-14 16:48:57 -0800

    update???

--- lab5/js/lab.js
+++ lab5/js/lab.js
@@ -8,7 +8,7 @@ var currentYear = 2021;
 var carAge = currentYear - carYear;
 var ownIt = "true";
 
-document.writeIn("Make: " + carMake + "<br>");
+document.write("Make: " + carMake + "<br>");
 document.writeIn("Model: " + carModel + "<br>");
 document.writeIn("Color: " + carColor + "<br>");
 document.writeIn("Year: " + carYear + "<br>");
