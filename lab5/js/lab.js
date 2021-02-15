e3dcfe9c3412140a96bc516ca7014ee4dab5414a
Victoria White <65001478+victoria-white@users.noreply.github.com>
2021-02-15 10:03:54 -0800

    Update lab.js

--- lab5/js/lab.js
+++ lab5/js/lab.js
@@ -8,8 +8,8 @@ var currentYear = 2021;
 var carAge = currentYear - carYear;
 var ownIt = "true";
 
-document.writeIn("Make: " + carMake + "<br>");
-document.writeIn("Model: " + carModel + "<br>");
-document.writeIn("Color: " + carColor + "<br>");
-document.writeIn("Year: " + carYear + "<br>");
-document.writeIn("Age: " + carAge + "Years<br>");
+document.writeln("Make: " + carMake + "<br>");
+document.writeln("Model: " + carModel + "<br>");
+document.writeln("Color: " + carColor + "<br>");
+document.writeln("Year: " + carYear + "<br>");
+document.writeln("Age: " + carAge + "Years<br>");
