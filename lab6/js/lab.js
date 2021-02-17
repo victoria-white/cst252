72ca969a76442216a3b540829ae9109128d79d4b
Victoria White <65001478+victoria-white@users.noreply.github.com>
2021-02-16 19:47:47 -0800

    Update Lab 6 (2/16)
    Updated Lab 6 content and images

---
+++ lab6/js/lab.js
@@ -0,0 +1,18 @@
+//Lab 6: Arrays and Objects
+//Author: Victoria White
+//Created: Feb. 15 2021
+//License: Public Domain
+
+var myTransport = ["Toyota Prius", "Walking", "Rides from friends", "Bike"];
+
+myMainRide = {
+  make: "Toyota",
+  model: "Prius",
+  year: 2008,
+  color: "Gray",
+  extras: "805 brewery sticker on the rear drivers side window",
+};
+
+document.writeln("Getting Around " + myTransport + "<br>");
+document.writeln("My Main Ride: <pre>"
+  JSON.stringify(myMainRide, null, '\t'), "<pre>");
