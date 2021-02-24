07fbcef8246cabdbc3f611616101ab29df1af7c6
Victoria White <65001478+victoria-white@users.noreply.github.com>
2021-02-23 17:17:47 -0800

    Update 2/23
    Update lab 7 html and js

---
+++ lab7/js/lab.js
@@ -0,0 +1,21 @@
+//Lab 7: Functions
+//Author: Victoria White
+//Created: Feb. 23 2021
+//License: Public Domain
+
+function myFunction() {
+    var userName = window.prompt("hi, what is your name?");
+    console.log("username = ", username);
+    //split string into array
+    var nameArray = userName.split('');
+    console.log("nameArray = ", nameArray);
+    //sort array
+    var nameArraySort = nameArray.sort();
+    console.log("nameArraySort = ", nameArraySort);
+    //convert array back into string
+    var nameSorted = nameArraySort.join('');
+    console.log("nameSorted = ", nameSorted);
+    return nameSorted;
+}
+document.writeln("fixed name: ",
+                 myFunction(), "<br>")
