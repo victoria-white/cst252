83ef803d77bfb10685a03c5bf247420be6386bd2
Victoria White <65001478+victoria-white@users.noreply.github.com>
2021-02-27 16:25:25 -0800

    Update 2/27
    Updated Lab 8 html and js as well as added two pictures

--- lab8/js/lab.js
+++ lab8/js/lab.js
@@ -8,21 +8,27 @@ function isEven (x){
   return (x % 2 == 0);
 }
 console.log("Lets test our 'isEven' function: ");
+document.writeln("Lets test our 'isEven' function: " + '<br>');
 
 //test function
 console.log("Is 4 even? ", isEven(4));
+document.writeln("Is 4 even? ", isEven(4) + '<br');
 console.log("Is 7 even? ", isEven(7));
+document.writeln("Is 7 even? ", isEven(7) + '<br');
 
 //define array
 var array = [2, 12, 3, 13, 4, 17];
 console.log("Here is our array: ", array);
+document.writeln("Here is our array: ", array + '<br>');
 
 //run array through function, sould return [true, true, false, false, true, false]
 var result = array.map(isEven);
 console.log("Are the numbers in our array even? ", result);
+document.writeln("Are the numbers in our array even? ", result + '<br>');
 
 //run calculation through Array sould return [4, 144, 9, 169, 16, 289]
 var result = array.map(function(x){
   return x**2;
 })
 console.log("Lets get the square of the array: ", result);
+document.writeln("Lets get the square of the array: ", result + '<br>');
