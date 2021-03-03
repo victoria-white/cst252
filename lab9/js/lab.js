200e953098a84b8477315aec94160255c8f14f72
Victoria White <65001478+victoria-white@users.noreply.github.com>
2021-03-02 21:11:05 -0800

    Update Lab 9
    added picture of Cooper as well as updated HTML and JS

---
+++ lab9/js/lab.js
@@ -0,0 +1,12 @@
+//Lab 9: Javascript for the Web
+//Author: Victoria White <vwhite@csumb.edu>
+//Created: Mar. 2 2021
+//License: Public Domain
+
+oldElement = document.getElementById("img");
+newElement = document.getElementById("div");
+oldElement.appendChild(newElement);
+newElement.id = "Cooper";
+newElement.innerHTML = "a chunky cat.";
+newElement.style.fontSize = "18pt";
+newElement.style.fontFamily = 'Tahoma';
