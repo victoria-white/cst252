6910a746ff1523f1f2c6dc25a2c4d161f8d950ca
Victoria White <65001478+victoria-white@users.noreply.github.com>
2021-03-07 21:02:11 -0800

    UPDATE 3/7
    updated lab 10 js

--- lab10/js/lab.js
+++ lab10/js/lab.js
@@ -2,3 +2,13 @@
 //Author: Victoria White <vwhite@csumb.edu>
 //Created: Mar. 7 2021
 //License: Public Domain
+
+var buttonEl = document.getElementById("my-button");
+console.log("El Button", buttonEl);
+
+buttonEl.addEventListener("click", function(){
+  var name = promt("what is your name?");
+  var titleEl = document.getElementById("user-name");
+  console.log("Title element: ", titleEl);
+  titleEl.innerText = "Hello, " + name;
+})
