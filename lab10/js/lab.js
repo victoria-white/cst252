1e2ae290de3ad712a8adc9787a9b73932d7b066c
Victoria White <65001478+victoria-white@users.noreply.github.com>
2021-03-07 21:15:34 -0800

    Update lab.js

--- lab10/js/lab.js
+++ lab10/js/lab.js
@@ -7,7 +7,7 @@ var buttonEl = document.getElementById("my-button");
 console.log("El Button", buttonEl);
 
 buttonEl.addEventListener("click", function(){
-  var name = promt("what is your name?");
+  var name = prompt("what is your name?");
   var titleEl = document.getElementById("user-name");
   console.log("Title element: ", titleEl);
   titleEl.innerText = "Hello, " + name;
