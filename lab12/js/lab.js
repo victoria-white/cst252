e144dfc3568fd6fdc24260f0d13be04a3e0cfeba
Victoria White <65001478+victoria-white@users.noreply.github.com>
2021-03-21 21:56:37 -0700

    update 3/21
    update lab 12 js

--- lab12/js/lab.js
+++ lab12/js/lab.js
@@ -3,7 +3,7 @@
 //Created: Mar. 10 2021
 //License: Public Domain
 
-//Javascript code courtesy of Wes Modes on iLearn
+//Javascript code courtesy of Wes Modes on iLearn and GitHub
 maxFactors = 4;
 
 outputEl = document.getElementById("output");
