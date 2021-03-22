3d5ae4fd06662aed1a6270e17ccb8b1d338b9e3b
Victoria White <65001478+victoria-white@users.noreply.github.com>
2021-03-21 21:34:07 -0700

    Update 3/21
    update lab 13 html and lab 12 js

--- lab12/js/lab.js
+++ lab12/js/lab.js
@@ -3,39 +3,65 @@
 //Created: Mar. 10 2021
 //License: Public Domain
 
-//given a number and an object that looks like this
-// {3: "Fizz", 5: "Buzz", 7: "Boom"}
-// Loops over the numbers and outputs the number and the matching text
-// for factors
-function fizzBuzzBoom(maxNums, factorObj) {
-  //iterate over all of out numbers
-  for (var num=0; num<maxNums; num++) {
-    //reset output string
-    var outputStr = "";
-    //iterate over the factors we got from the HTML
-    for (var factor in factorObj) {
-      //check to see if this num is a multiple of factor
-      if (num % factor == 0) {
-        //if yes, then ass the text to output string
-        outputStr += factorObj[factor];
-      }
+maxFactors = 4;
+
+outputEl = document.getElementById("output");
+
+function getFactorObj() {
+  var = factorObj {};
+  for (var factor = 0; factor<maxFactors; factor++) {
+    numId = "num" + factor;
+    textId = "text" + factor;
+    numValue = document.getElementById(numId).value;
+    textValue = document.getElementById(textId).value;
+    console.log(factor + "num: ", numValue, "text: ", textValue);
+    if (numValue && textValue) {
+      factorObj[numValue] = textValue;
     }
-    //now if we have words in outputStr, format it like this " - FixxBuzz!"
-    if (outputStr) {
-      outputStr = " - " + outputStr + "!";
-    }
-    outputToPage(num.toString() + outputStr)
   }
+  return factorObj;
+}
+
+function outputToPage(str) {
+  newEL = document.createElement("p");
+  newEl.innerHTML = str;
+  outputEl.appendChild(newEl);
+}
+
+unction fizzBuzzBoom(maxNums, factorObj) {
+    for (var num=0; num<maxNums; num++) {
+        var outputStr = "";
+        for (var factor in factorObj) {
+            if (num % factor == 0) {
+                outputStr += factorObj[factor];
+            }
+        }
+        if (outputStr) {
+            outputStr = " - " + outputStr + "!";
+        }
+        outputToPage(num.toString() + outputStr)
+    }
 }
 
 function reportError(str) {
-  outputEl.innerHTML = "<div class='error'>" + str + "</div>";
+    outputEl.innerHTML = "<div class='error'>" + str + "</div>";
 }
 
 document.getElementById("submit").addEventListener("click", function() {
-  var max = document.getElementById("max").value;
-  console.log("max: ", max);
-  if (! max){
-    reportError("You Must Provide a Maximum");
-  }
+    var max = document.getElementById("max").value;
+    console.log("max:", max);
+    if (! max) {
+        reportError("You must provide a maximum");
+        return;
+    }
+    var factorObj = getFactorObj();
+    console.log("factorObj:", factorObj);
+    if (Object.keys(factorObj).length === 0) {
+        reportError("You must provide at least one factor and text");
+        return;
+    }
+    
+    outputEl.innerHTML = "";
+    fizzBuzzBoom(max, factorObj);
+    outputEl.classList.add("cols");
 })
