ac3a5c261f3c88103eba4a67af45c51f394e7e76
Victoria White <65001478+victoria-white@users.noreply.github.com>
2021-03-24 14:57:46 -0700

    Update 3/24
    created lab 15 and updated html and js

---
+++ lab15/js/lab.js
@@ -0,0 +1,17 @@
+//Lab 15: ???
+//Author: Victoria White <vwhite@csumb.edu>
+//Created: Mar. 24 2021
+//License: Public Domain
+
+class Vehicle {
+  constructor(make, model, year, color, extras, name) {
+    this.make = make;
+    this.model = model;
+    this.year = year;
+    this.color = color;
+    this.extras = extras;
+    this.name = name;
+  }
+  info() {
+    return this.color + " " + this.year + " " + this.make + " " +  this.model + " with " +  this.extras + " called " + this.name;
+  }
