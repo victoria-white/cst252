b40eea0bba67b1ea5d91c4479299c66a97260b22
Victoria White <65001478+victoria-white@users.noreply.github.com>
2021-03-23 21:35:14 -0700

    update 3/23
    updated lab 14 html and js

--- lab14/js/lab.js
+++ lab14/js/lab.js
@@ -2,3 +2,17 @@
 //Author: Victoria White <vwhite@csumb.edu>
 //Created: Mar. 23 2021
 //License: Public Domain
+
+function DreamCar(make, model, year, color) {
+  this.carMake = make;
+  this.carModel = model;
+  this.carYear = year;
+  this.carColor= color;
+}
+
+DreamCar.prototype.name = function() {
+  return this.carMake + " " + this.carModel
+};
+
+var myDreamCar = new DreamCar("Toyota", "Tacoma", 2010, "Green");
+document.getElementById("demo").innerHTML = "My Dream Car is a " + myDreamCar.name();
