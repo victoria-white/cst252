5e0449eed992a2a8710f789ed78f3c9abfd1c9be
Victoria White <65001478+victoria-white@users.noreply.github.com>
2021-04-05 06:19:15 -0700

    Update 4/5
    Update Lab 15 HTML and Js

--- lab15/js/lab.js
+++ lab15/js/lab.js
@@ -14,4 +14,17 @@ class Vehicle {
   }
   info() {
     return this.color + " " + this.year + " " + this.make + " " +  this.model + " with " +  this.extras + " called " + this.name;
+    }
+  }
+
+  var vehicles = [];
+
+  vehicles.push(new Vehicle("Toyota", "Prius", 2008, "gray", "thule roof racks", "Torie's Car"));
+  vehicles.push(new Vehicle("Toyota", "Tacoma", 2010, "green", "camper shell", "dream car"));
+
+  outputEl = document.getElementById("output");
+  for (var i = 0; i<vehicles.length; i++) {
+    var newEl = document.createElement("li");
+    newEl.innerText = vheicles[i].info();
+    outputEl.appendChild(newEl);
   }
