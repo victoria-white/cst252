af05f757bbb565d9b570d22dedb6be4e2dd9a0e0
Victoria White <65001478+victoria-white@users.noreply.github.com>
2021-04-05 06:50:29 -0700

    Update 4/5
    created game.js and updated lab 15 html

---
+++ lab15/js/game.js
@@ -0,0 +1,26 @@
+// Lab 15: game.js
+// Use Javascript to Pseudocode a hangman game
+// Author: Victoria White
+// Created: April 2, 2021
+// License: Public Domain
+
+//HangMan
+  //Create a Hangman Board 
+  //Computer alays chooses vocabulary
+  //Win condition - player guesses word correctly
+  //Lose condition - player guesses too many wrong answers and makes a complete stick figure
+
+  //Set up the game initially (HangMan)
+  //    Create a clickable button alphabet [Alphabet]
+  //    Create blank spaces that will be filled in by letters [Blank Space]
+  //    Create a hangman object [Stick Figure (6 pieces total = 6 guesses)]
+  //    Create players [Player]
+  //    Create vocabulary [Vocab]
+  //Start the game loop [HangMan]
+  //    Render the board [Board]
+  //    Ask the player to click a letter [Click]
+  //If the player wins [Winner]
+  //    Show full word with text "YOU WIN!!!"
+  //If the player loses [Loser]
+  //    Show full word and hanged stick figure with text "YOU LOSE"
+//End HangMan
