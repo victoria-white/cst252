afffeac3834db023f15c8e7126f7d761d21fa8b4
Victoria White <65001478+victoria-white@users.noreply.github.com>
2021-04-06 22:45:50 -0700

    Update 4/6
    updated lab 16 and 17

---
+++ lab16/js/lab.js
@@ -0,0 +1,14 @@
+//Lab 16: Libraries & Intro to jQuery
+//Author: Victoria White <vwhite@csumb.edu>
+//Created: April 6th 2021
+//License: Public Domain
+
+button - $("output").append("<button id-button class-button> A Button");
+
+but = $("#button");
+
+$(".button").css("color", "green");
+
+but.click(function() {
+  alert("Thanks for clicking me!! :)");
+});
---
+++ lab17/js/lab.js
@@ -0,0 +1,4 @@
+//Lab 17: Advanced jQuery
+//Author: Victoria White <vwhite@csumb.edu>
+//Created: April 6th 2021
+//License: Public Domain
