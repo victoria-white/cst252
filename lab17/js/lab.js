5aca008838a510f0107bd46fc385cf50af923ec7
Victoria White <65001478+victoria-white@users.noreply.github.com>
2021-04-11 20:04:19 -0700

    Update 4/11
    Updated lab 17 html, js, and added dr seuss jpg

--- lab17/js/lab.js
+++ lab17/js/lab.js
@@ -2,3 +2,22 @@
 //Author: Victoria White <vwhite@csumb.edu>
 //Created: April 6th 2021
 //License: Public Domain
+
+//Code from Wes Modes during class
+
+//Add event listener to button (1)
+$("#thing1-button").click(function(){
+  $("#thing2-button").toggleClass("Name1");
+})
+
+//Add event listener to button (2)
+$("#thing1-button").click(function(){
+  $("#thing2-button").toggleClass("Name2");
+})
+
+$("#submit").click(function(){
+  var = fullname = $("#fullname").val();
+  $("replace").html(fullname + "'s");
+  $("#thing1-button").html(fullname + " 1");
+  $("#thing2-button").html(fullname + " 2");
+})
